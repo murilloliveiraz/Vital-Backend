17a87063cec43bb20466a65e25bac6edc1690418
Murillo Oliveira <murillo1047@gmail.com>
2024-08-27 14:41:15 -0300

    feat: methods and classes to help database services

---
+++ Infraestructure/Helpers/MigrationHelper.cs
@@ -0,0 +1,27 @@
+﻿using Infraestructure.Contexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Helpers
+{
+    public static class MigrationHelper
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            using ApplicationContext context =
+                scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+
+            try
+            {
+                context.Database.Migrate();
+                Console.WriteLine("Migrations applied successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error applying migrations: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Infraestructure/Helpers/MongoDatabaseConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Infraestructure.Helpers
+{
+    public class MongoDatabaseConfig
+    {
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
