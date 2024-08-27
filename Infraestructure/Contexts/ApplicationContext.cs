a28c1c69c568bf26b8524f481ab9f5156e772480
Murillo Oliveira <murillo1047@gmail.com>
2024-08-27 14:40:44 -0300

    feat: application context

---
+++ Infraestructure/Contexts/ApplicationContext.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using Infraestructure.ClassMappings;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Contexts
+{
+    public class ApplicationContext : IdentityDbContext<Usuario>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Administrador> Administradores { get; set; }
+        public DbSet<Hospital> Hospitais { get; set; }
+        public DbSet<HospitalServico> HospitalServicos { get; set; }
+        public DbSet<Servico> Servicos { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Exame> Exames { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AdminMap());
+            modelBuilder.ApplyConfiguration(new HospitalMap());
+            modelBuilder.ApplyConfiguration(new HospitalServicoMap());
+            modelBuilder.ApplyConfiguration(new ServicoMap());
+            modelBuilder.ApplyConfiguration(new MedicoMap());
+            modelBuilder.ApplyConfiguration(new ExameMap());
+            modelBuilder.ApplyConfiguration(new PacienteMap());
+            base.OnModelCreating(modelBuilder);
+        }  
+        public string ConnectionString()
+        {
+            return "Host=localhost;Port=5432;Database=hospital;Username=postgres;Password=123456;";
+
+        }
+    }
+}
