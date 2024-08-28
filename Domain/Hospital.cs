86136217c43cd4e6a9077989e33c37ff7e19892d
Murillo Oliveira <murillo1047@gmail.com>
2024-08-28 09:14:43 -0300

    refactor: adding 'estado' property to filter in the future

--- Domain/Hospital.cs
+++ Domain/Hospital.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Endereco { get; set; }
+        public string Estado { get; set; }
         public string Telefone { get; set; }
         public DateTime? DataInativacao { get; set; }
         public ICollection<HospitalServico> Servicos { get; set; }
