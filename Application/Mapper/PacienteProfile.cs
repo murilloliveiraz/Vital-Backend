b7a6067a3bf86f1aa41d95c6a8d644075682ed7a
Murillo Oliveira <murillo1047@gmail.com>
2024-09-02 16:38:05 -0300

    feat: add Patient mapping and controller
    - Create PacienteProfile for automatic entity mapping
    - Implement PacienteController to manage patient-related endpoints

---
+++ Application/Mapper/PacienteProfile.cs
@@ -0,0 +1,27 @@
+﻿using Application.DTOS.Paciente;
+using Application.DTOS.Usuario;
+using AutoMapper;
+using Domain;
+
+namespace Application.Mapper
+{
+    public class PacienteProfile : Profile
+    {
+        public PacienteProfile()
+        {
+            CreateMap<Paciente, UsuarioRequestContract>().ReverseMap();
+            CreateMap<Paciente, UsuarioResponseContract>().ReverseMap();
+            CreateMap<Paciente, PacienteRequestContract>().ReverseMap();
+            CreateMap<Paciente, PacienteResponseContract>()
+                .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.Usuario.Nome))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Usuario.Email))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Usuario.Id))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Usuario.Role))
+                .ForMember(dest => dest.CPF, opt => opt.MapFrom(src => src.Usuario.CPF))
+                .ForMember(dest => dest.DataCriacao, opt => opt.MapFrom(src => src.Usuario.DataCriacao))
+                .ForMember(dest => dest.DataInativacao, opt => opt.MapFrom(src => src.Usuario.DataInativacao))
+                .ForMember(dest => dest.CriadoPorUsuarioId, opt => opt.MapFrom(src => src.Usuario.CriadoPorUsuarioId))
+                .ReverseMap();
+        }
+    }
+}
---
+++ VitalAPI/Controllers/PacienteController.cs
@@ -0,0 +1,71 @@
+﻿using Application.DTOS.Paciente;
+using Application.Services.Classes;
+using Application.Services.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VitalAPI.Controllers
+{
+    [Route("api/pacientes")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+        private readonly IPacienteService _pacienteService;
+
+        public PacienteController(IPacienteService pacienteService)
+        {
+            _pacienteService = pacienteService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PacienteRequestContract model)
+        {
+            var paciente = await _pacienteService.Create(model);
+            return CreatedAtAction(nameof(GetById), new { id = paciente.Id }, paciente);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _pacienteService.Delete(id);
+            return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _pacienteService.Get());
+        }
+
+        [HttpGet("pesquisar/cpf")]
+        public async Task<IActionResult> GetByCPF(string cpf)
+        {
+            return Ok(await _pacienteService.GetByCPF(cpf));
+        }
+
+        [HttpGet("pesquisar/email")]
+        public async Task<IActionResult> GetByEmail(string email)
+        {
+            return Ok(await _pacienteService.GetByEmail(email));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _pacienteService.GetById(id));
+        }
+
+        [HttpGet("pesquisar/nome")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            return Ok(await _pacienteService.GetByName(name));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, PacienteRequestContract model)
+        {
+            await _pacienteService.Update(id, model);
+            return Ok();
+        }
+    }
+}
