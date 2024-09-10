d3d0a5667b3efa37d29893a1a3a47b0f93bd7749
Murillo Oliveira <murillo1047@gmail.com>
2024-09-10 16:41:03 -0300

    refactor: updated s3storage service

--- Infraestructure/Services/Interfaces/IS3StorageService.cs
+++ Infraestructure/Services/Interfaces/IS3StorageService.cs
@@ -5,7 +5,7 @@ namespace Infraestructure.Services.Interfaces
 {
     public interface IS3StorageService
     {
-        Task<S3UploadResult> UploadFileAsync(IFormFile file, string prefix);
-        Task<S3FileResult> GetFileByKeyAsync(string key);
+        Task<S3UploadResult> UploadFileAsync(IFormFile file, string prefix, string bucketName);
+        Task<S3FileResult> GetFileByKeyAsync(string key, string bucketName);
     }
 }
