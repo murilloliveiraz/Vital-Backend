9ecbb788420df3766314e912ddb83aca3be22662
Murillo Oliveira <murillo1047@gmail.com>
2024-09-10 16:40:09 -0300

    feat: created method to convert file to base64

---
+++ Infraestructure/Services/Helpers/Base64FileConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace Infraestructure.Helpers
+{
+    public static class Base64FileConverter
+    {
+        public static string ConvertToBase64(Stream fileStream)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                fileStream.CopyTo(memoryStream);
+                var fileBytes = memoryStream.ToArray();
+                return Convert.ToBase64String(fileBytes);
+            }
+        }
+    }
+}
