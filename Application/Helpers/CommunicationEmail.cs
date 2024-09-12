8efb069cd26d775f22f670f11866e17901a55b33
Murillo Oliveira <murillo1047@gmail.com>
2024-09-12 16:15:36 -0300

    feat: emails to confirm appointment, reset password and alert patient that result is available

--- Application/Helpers/CommunicationEmail.cs
+++ Application/Helpers/CommunicationEmail.cs
@@ -12,5 +12,32 @@
             Response += "</div>";
             return Response;
         }
+        public static string ResultAvailableEmail(string portalUrl)
+        {
+            string response = "<div style=\"width:100%;background-color:#FFAC1C;text-align:center;margin:10px\">";
+            response += "<h1>Resultado Disponível</h1>";
+            response += "<p>Olá,</p>";
+            response += "<p>Seu resultado está disponível no portal. Por favor, clique no link abaixo para acessar:</p>";
+            response += $"<a href='{portalUrl}' style='color:white;background-color:#007BFF;padding:10px;text-decoration:none;border-radius:5px;'>Acessar Portal</a>";
+            response += "<p>Obrigado por usar nossos serviços.</p>";
+            response += "<div><h3>vitaltech@gmail.com</h3></div>";
+            response += "</div>";
+            return response;
+        }
+
+        public static string AppointmentConfirmationEmail(string appointmentDetails)
+        {
+            string response = "<div style=\"width:100%;background-color:#FFAC1C;text-align:center;margin:10px\">";
+            response += "<h1>Consulta Agendada</h1>";
+            response += "<p>Olá,</p>";
+            response += "<p>Sua consulta foi agendada com sucesso!</p>";
+            response += $"<p>Detalhes da consulta: {appointmentDetails}</p>";
+            response += "<p>Se você precisar alterar ou cancelar a consulta, entre em contato com a clínica.</p>";
+            response += "<p>Estamos ansiosos para atendê-lo!</p>";
+            response += "<div><h3>vitaltech@gmail.com</h3></div>";
+            response += "</div>";
+            return response;
+        }
+
     }
 }
