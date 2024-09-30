4e8985ed8dcf050e2335d7de42086acc42490684
Murillo Oliveira <murillo1047@gmail.com>
2024-09-30 15:37:42 -0300

    feat: adding credit card service to accept payments

---
+++ Application/Services/Classes/CreditCardService.cs
@@ -0,0 +1,41 @@
+﻿using Application.Services.Interfaces;
+using Domain;
+using MercadoPago.Client.Common;
+using MercadoPago.Client.Payment;
+using MercadoPago.Client;
+using MercadoPago.Resource.Payment;
+
+namespace Application.Services.Classes
+{
+    public class CreditCardService : ICreditCardService
+    {
+        public async Task<Payment> CobrarComPix(CreditCardPayment request)
+        {
+            var requestOptions = new RequestOptions();
+            string idempotencyKey = Guid.NewGuid().ToString();
+            requestOptions.CustomHeaders.Add("x-idempotency-key", idempotencyKey);
+
+            var paymentRequest = new PaymentCreateRequest
+            {
+                TransactionAmount = request.TransactionAmount,
+                Token = request.Token,
+                Description = request.Description,
+                Installments = request.Installments,
+                PaymentMethodId = request.PaymentMethodId,
+                Payer = new PaymentPayerRequest
+                {
+                    Email = request.Email,
+                    Identification = new IdentificationRequest
+                    {
+                        Type = request.Type,
+                        Number = request.Number,
+                    },
+                },
+            };
+
+            var client = new PaymentClient();
+            Payment payment = await client.CreateAsync(paymentRequest, requestOptions);
+            return payment;
+        }
+    }
+}
---
+++ Application/Services/Interfaces/ICreditCardService.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+using MercadoPago.Resource.Payment;
+
+namespace Application.Services.Interfaces
+{
+    public interface ICreditCardService
+    {
+        Task<Payment> CobrarComPix(CreditCardPayment request);
+    }
+}
