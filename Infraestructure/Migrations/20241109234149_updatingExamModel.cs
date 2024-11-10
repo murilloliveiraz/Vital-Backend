21580236cb335b11069bb8cd1af93c5c1908b7a9
Murillo Oliveira <murillo1047@gmail.com>
2024-11-09 21:30:09 -0300

    refactor: changing models to adapt new business logic

---
+++ Infraestructure/Migrations/20241109234149_updatingExamModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatingExamModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UrlResultadoClinicaExterna",
+                table: "exames",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlResultadoClinicaExterna",
+                table: "exames");
+        }
+    }
+}
