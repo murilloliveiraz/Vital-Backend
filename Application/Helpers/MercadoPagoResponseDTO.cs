b3475374266e661aba0cae12fa5b7be8c6ef6efc
Murillo Oliveira <murillo1047@gmail.com>
2024-11-15 20:40:24 -0300

    fix: fixing type of properties

--- Application/Helpers/MercadoPagoResponseDTO.cs
+++ Application/Helpers/MercadoPagoResponseDTO.cs
@@ -2,11 +2,11 @@
 {
     public class MercadoPagoResponseDTO
     {
-            public int id { get; set; }
+            public long id { get; set; }
             public bool live_mode { get; set; }
             public string type { get; set; }
             public string date_created { get; set; }
-            public int userid { get; set; }
+            public string userid { get; set; }
             public string api_version { get; set; }
             public string action { get; set; }
             public Data data { get; set; }
