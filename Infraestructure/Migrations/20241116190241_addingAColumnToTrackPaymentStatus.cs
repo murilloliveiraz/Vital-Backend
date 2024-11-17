f1719a4fe0d285af51666cdc7ad1ecd4e848697d
Murillo Oliveira <murillo1047@gmail.com>
2024-11-16 21:19:31 -0300

    feat: new coolumn to track payment status

---
+++ Infraestructure/Migrations/20241116190241_addingAColumnToTrackPaymentStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingAColumnToTrackPaymentStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StatusPagamento",
+                table: "exames",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusPagamento",
+                table: "exames");
+        }
+    }
+}
